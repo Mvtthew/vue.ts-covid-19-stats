8e7ae315dd1761c6fa16bef21410cd492b84990a
Mateusz Ozog <mateusz2345678@gmail.com>
2020-04-03 21:30:21 +0200

    :pencil: Svelte -> Vue

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+	<div id="app">
+		<navbar></navbar>
+		<div class="container">
+			<p>HA</p>
+		</div>
+	</div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import Navbar from "./components/Navbar.vue";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+	components: { Navbar }
 })
 export default class App extends Vue {}
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import "../node_modules/bootstrap/scss/bootstrap.scss"
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		<div class="container">
+			<a class="navbar-brand" href="/">COVID-19 Stats</a>
+			<div class="ml-auto" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="/">Home</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Navbar extends Vue {}
+</script>
